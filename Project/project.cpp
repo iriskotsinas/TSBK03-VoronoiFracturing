07c4a57783e2255a0f93eaafedc7a70c54d78d45
Viktor Tholén <viktor.tholen@gmail.com>
2021-11-03 09:59:27 +0100

    working skeleton linux

--- Project/project.cpp
+++ Project/project.cpp
@@ -1,15 +1,10 @@
-// Lab 1-1, multi-pass rendering with FBOs and HDR.
-// Revision for 2013, simpler light, start with rendering on quad in final stage.
-// Switched to low-res Stanford Bunny for more details.
-// No HDR is implemented to begin with. That is your task.
-
-// 2018: No zpr, trackball code added in the main program.
-// 2021: Updated to use LittleOBJLoader.
-
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-
+#include <cstddef>
+#include <stddef.h>
+// Include GLEW
+#include <GL/glew.h>
 #ifdef __APPLE__
 // Mac
 	#include <OpenGL/gl3.h>
@@ -24,262 +19,136 @@
 		#include <GL/glut.h>
 	#else
 // Linux
-		#include <stdio.h>
+		// #include <stdio.h>
 		#include <GL/gl.h>
-		#include "MicroGlut.h"
+		// #include "MicroGlut.h"
 		// #include <GL/glut.h>
 	#endif
 #endif
 
-#include "VectorUtils3.h"
-#include "GL_utilities.h"
-#include "LittleOBJLoader.h"
+// #include "VectorUtils3.h"
+// #include "GL_utilities.h"
+// #include "LittleOBJLoader.h"
 #include <glm/vec3.hpp> 
 // #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 // #include "imgui/imgui.h"
 
-// initial width and heights
-#define W 512
-#define H 512
+// Include GLFW
+#include <GLFW/glfw3.h>
 
-#define NUM_LIGHTS 4
+#include <iostream>
 
-void OnTimer(int value);
+#include "src/VertexBuffer.h"
+#include "src/VertexArray.h"
+#include "src/IndexBuffer.h"
+#include "src/Shader.h"
+#include "src/Renderer.h"
 
-mat4 projectionMatrix;
-mat4 viewMatrix, modelToWorldMatrix;
-
-
-GLfloat square[] = {
-							-1,-1,0,
-							-1,1, 0,
-							1,1, 0,
-							1,-1, 0};
-GLfloat squareTexCoord[] = {
-							 0, 0,
-							 0, 1,
-							 1, 1,
-							 1, 0};
-GLuint squareIndices[] = {0, 1, 2, 0, 2, 3};
-
-Model* squareModel;
-
-//----------------------Globals-------------------------------------------------
-Model *model1;
-FBOstruct *fbo1, *fbo2, *fbo3;
-GLuint phongshader = 0, plaintextureshader = 0, lowpass = 0, truncate = 0, merger = 0;
-
-//-------------------------------------------------------------------------------------
-void init(void)
+GLFWwindow* InitWindow()
 {
-    // glm::vec3 hello;
-	dumpInfo();  // shader info
-
-	// GL inits
-	glClearColor(0.1, 0.1, 0.3, 0);
-	glClearDepth(1.0);
-	glEnable(GL_DEPTH_TEST);
-	glDisable(GL_CULL_FACE);
-	printError("GL inits");
-
-	// Load and compile shaders
-	plaintextureshader = loadShaders("plaintextureshader.vert", "plaintextureshader.frag");  // puts texture on teapot
-	phongshader = loadShaders("phong.vert", "phong.frag");  // renders with light (used for initial renderin of teapot)
-	printError("init shader");
-
-	fbo1 = initFBO(W, H, 0);
-	fbo2 = initFBO(W, H, 0);
-	fbo3 = initFBO(W, H, 0);
-
-	// load the model
-//	model1 = LoadModelPlus("teapot.obj");
-	model1 = LoadModelPlus("stanford-bunny.obj");
-
-	squareModel = LoadDataToModel(
-			(vec3 *)square, NULL, (vec2 *)squareTexCoord, NULL,
-			squareIndices, 4, 6);
-
-	vec3 cam = SetVector(0, 5, 15);
-	vec3 point = SetVector(0, 1, 0);
-	vec3 up = {0, 1, 0};
-	viewMatrix = lookAtv(cam, point, up);
-	modelToWorldMatrix = IdentityMatrix();
-
-	// glutTimerFunc(5, &OnTimer, 0);
-
-//	zprInit(&viewMatrix, cam, point);
-}
-
-void OnTimer(int value)
-{
-	glutPostRedisplay();
-	glutTimerFunc(5, &OnTimer, value);
-}
-
-//-------------------------------callback functions------------------------------------------
-void display(void)
-{
-	mat4 vm2;
-
-	// This function is called whenever it is time to render
-	//  a new frame; due to the idle()-function below, this
-	//  function will get called several times per second
-
-	// render to fbo1!
-	useFBO(fbo1, 0L, 0L);
-
-	// Clear framebuffer & zbuffer
-	glClearColor(0.1, 0.1, 0.3, 0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	// Activate shader program
-	glUseProgram(phongshader);
-
-	vm2 = Mult(viewMatrix, modelToWorldMatrix);
-	// Scale and place bunny since it is too small
-	vm2 = Mult(vm2, T(0, -8.5, 0));
-	vm2 = Mult(vm2, S(80,80,80));
-
-	glUniformMatrix4fv(glGetUniformLocation(phongshader, "projectionMatrix"), 1, GL_TRUE, projectionMatrix.m);
-	glUniformMatrix4fv(glGetUniformLocation(phongshader, "modelviewMatrix"), 1, GL_TRUE, vm2.m);
-	// glUniform3fv(glGetUniformLocation(phongshader, "camPos"), 1, &cam.x);
-	glUniform1i(glGetUniformLocation(phongshader, "texUnit"), 0);
-	// Enable Z-buffering
-	glEnable(GL_DEPTH_TEST);
-	// Enable backface culling
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_BACK);
-
-	DrawModel(model1, phongshader, "in_Position", "in_Normal", NULL);
-
-	// Done rendering the FBO! Set up for rendering on screen, using the result as texture!
-
-//  glFlush(); // Can cause flickering on some systems. Can also be necessary to make drawing complete.
-
-	useFBO(0L, fbo1, 0L);
-	glClearColor(0.0, 0.0, 0.0, 0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	// Activate second shader program
-	glUseProgram(plaintextureshader);
-
-	glDisable(GL_CULL_FACE);
-	glDisable(GL_DEPTH_TEST);
-	DrawModel(squareModel, plaintextureshader, "in_Position", NULL, "in_TexCoord");
-	glFlush();
-	// glutSwapBuffers();
-}
-
-void reshape(GLsizei w, GLsizei h)
-{
-	glViewport(0, 0, w, h);
-	GLfloat ratio = (GLfloat) w / (GLfloat) h;
-	projectionMatrix = perspective(90, ratio, 1.0, 1000);
-}
-
-
-// This function is called whenever the computer is idle
-// As soon as the machine is idle, ask GLUT to trigger rendering of a new
-// frame
-void idle()
-{
-  glutPostRedisplay();
-}
-
-// Trackball
-
-int prevx = 0, prevy = 0;
-
-void mouseUpDown(int button, int state, int x, int y)
-{
-	if (state == GLUT_DOWN)
-	{
-		prevx = x;
-		prevy = y;
-	}
-}
-
-void mouseDragged(int x, int y)
-{
-	vec3 p;
-	mat4 m;
-
-	// This is a simple and IMHO really nice trackball system:
-
-	// Use the movement direction to create an orthogonal rotation axis
-
-	p.y = x - prevx;
-	p.x = -(prevy - y);
-	p.z = 0;
-
-	// Create a rotation around this axis and premultiply it on the model-to-world matrix
-	// Limited to fixed camera! Will be wrong if the camera is moved!
-
-	m = ArbRotate(p, sqrt(p.x*p.x + p.y*p.y) / 50.0); // Rotation in view coordinates
-	modelToWorldMatrix = Mult(m, modelToWorldMatrix);
-
-	prevx = x;
-	prevy = y;
-
-	glutPostRedisplay();
-}
-
-
-//-----------------------------main-----------------------------------------------
-int main(int argc, char *argv[])
-{
-	// glutInit(&argc, argv);
-
-	// glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-	// glutInitWindowSize(W, H);
-
-	// glutInitContextVersion(3, 2);
-	// glutCreateWindow ("Render to texture with FBO");
-	// glutDisplayFunc(display);
-	// glutReshapeFunc(reshape);
-	// glutMouseFunc(mouseUpDown);
-	// glutMotionFunc(mouseDragged);
-	// glutIdleFunc(idle);
-
-	// init();
-	// glutMainLoop();
-	// exit(0);
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    // Initialise GLFW
+    if( !glfwInit() )
     {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        getchar();
+        return nullptr;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window = glfwCreateWindow( 1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
+    if( window == NULL ){
+        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+        getchar();
         glfwTerminate();
-        return -1;
+        return nullptr;
+
     }
-
-
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
-	// gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-
-	init();
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        display();
-        /* Render here */
-        // glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+    //Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return nullptr;
     }
-	glfwDestroyWindow(window);
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
+
+    std::cout << "Using GL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    return window;
 }
+
+int main( void )
+{
+    GLFWwindow* window = InitWindow();
+    if (!window)
+        return -1;
+
+    float positions[] = {
+        -0.5f, -0.5f, // 0
+         0.5f, -0.5f, // 1
+         0.5f,  0.5f, // 2
+        -0.5f,  0.5f  // 3
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    {
+        VertexArray va;
+        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        IndexBuffer ib(indices, 6);
+
+        VertexBufferLayout layout;
+        layout.AddFloat(2);
+
+        va.AddBuffer(vb, layout);
+
+        Shader shader("src/res/shaders/Basic.shader");
+        shader.Bind();
+
+        float red = 0.0f;
+        float step = 0.05f;
+
+        Renderer renderer;
+
+        do {
+            renderer.Clear();
+            shader.Bind();
+            shader.SetUniform4f("u_Color", red, 0.3, 0.8, 1.0);
+
+            renderer.Draw(va, ib, shader);
+
+            // Swap buffers
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+
+            // increment red
+            if (red < 0.0f || red > 1.0f)
+                step *= -1.0;
+            red += step;
+
+        } // Check if the ESC key was pressed or the window was closed
+        while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+                glfwWindowShouldClose(window) == 0 );
+    }
+
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
+
+    return 0;
+}
+
